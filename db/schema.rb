aa06fa08693281b9f11c2d0a9d2036cd448144be
TruongNM <truongkechun@gmail.com>
2017-09-26 10:44:19 +0700

    Remove photos with ajax

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20170918154905) do
 
   create_table "photos", force: :cascade do |t|
     t.integer "room_id"
-    t.string "image_url"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_photos_on_room_id"
