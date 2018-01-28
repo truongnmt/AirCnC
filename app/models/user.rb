19e7bc18fe9910ecae72a9e87830f5e9966a8e76
TruongNM <truongkechun@gmail.com>
2018-01-28 19:15:13 +0700

    SMS Configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,23 @@ class User < ApplicationRecord
       end
     end
   end
+
+  def generate_pin
+    self.pin = SecureRandom.hex(2)
+    self.phone_verified = false
+    save
+  end
+
+  def send_pin
+    @client = Twilio::REST::Client.new
+    @client.messages.create(
+      from: '12138954743',
+      to: self.phone_number,
+      body: "Your pin is #{self.pin}"
+    )
+  end
+
+  def verify_pin entered_pin
+    update(phone_verified: true) if self.pin == entered_pin
+  end
 end
