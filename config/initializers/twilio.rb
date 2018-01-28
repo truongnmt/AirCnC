4bb2afb95177302099bbd6dba1da1b75116278b7
TruongNM <truongkechun@gmail.com>
2018-01-28 20:55:33 +0700

    sms controller

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
 Twilio.configure do |config|
-  config.account_sid = 'ACcf5fed6a0e5db6d48b690c4602ffcea7'
-  config.auth_token = '364212b2a4d66027b62601a8d5ddb6ce'
+  config.account_sid = ENV["TWILIO_SID"]
+  config.auth_token = ENV["TWILIO_AUTH_TOKEN"]
 end
